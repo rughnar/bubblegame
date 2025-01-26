a7fc6c71a5ba7267f4aab092d4959cd2df1c8747
ioelion <rughnar@protonmail.com>
2025-01-26 19:44:52 -0300

    Tienda parte 2

--- Assets/ShopController.cs
+++ Assets/ShopController.cs
@@ -5,14 +5,26 @@ using UnityEngine;
 
 public class ShopController : MonoBehaviour
 {
+    public AudioClip openShop;
+    public List<AudioClip> buyAudios;
+
     public TMP_Text playerMoneyText;
     private GameManager gameManager;
     private float playerMoney;
+    private AudioManager audioManager;
     void Awake()
     {
+        audioManager = FindObjectOfType<AudioManager>();
         gameManager = FindObjectOfType<GameManager>();
         playerMoney = gameManager.GetPlayerMoney();
         playerMoneyText.text = "" + playerMoney;
+        audioManager.PlaySFX(openShop);
+
+    }
+
+    void Buy()
+    {
+        audioManager.PlaySFX(buyAudios[Random.Range(0, buyAudios.Count)]);
     }
 
 }
